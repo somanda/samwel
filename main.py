fe27bb380f16826c15a18d8673b502e16a886149
somanda <somanda2000@yahoo.fr>
2019-06-08 22:15:37 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+This file contain the informations about the song Brigatier Sabari of the artist Alpha Blony. It can be used to print all those informations
+"""
+#The song's title
+Title = "Brigadier Sabari"
+
+#name of the artist
+Artist = "Apha Blondy"
+
+#The  name of the Album
+Album = "Jah Gory"
+
+#The genre of the music
+Genre = "Raggae"
+
+#The year the song was released
+YearReleased = 1982
+
+#The label of the production
+Label = "Syllart Record"
+
+# The name of the producer
+Producer = "Georges Taï Benson"
+
+#The duration of the song
+Duration_sec = 285.00
+
+#Lhe language of the song
+Language = "Dioula"
+
+print(Title)
+print(Artist)
+print(Album)
+print(Genre)
+print(YearReleased)
+print(Label)
+print(Producer)
+print(Duration_sec)
+print(Language)
